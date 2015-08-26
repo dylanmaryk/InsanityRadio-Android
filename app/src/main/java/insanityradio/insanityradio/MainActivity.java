a46a8ab40352433715141cc10027e155860717e8
Dylan Maryk <dylan@dylanmaryk.com>
2015-08-26 23:26:42 +0200

    Add status bar colour.

--- app/src/main/java/insanityradio/insanityradio/MainActivity.java
+++ app/src/main/java/insanityradio/insanityradio/MainActivity.java
@@ -1,12 +1,15 @@
 package insanityradio.insanityradio;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.socialize.CommentUtils;
 import com.socialize.entity.Entity;
@@ -17,6 +20,13 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.InsanityOrangeDark));
+        }
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         viewPager.setAdapter(new PagerAdapter(getSupportFragmentManager()));

12c2612e9e7c610ef192ea981b56ba7662e0386a
Dylan Maryk <dylan@dylanmaryk.com>
2015-09-02 21:19:44 +0200

    Get basic radio streaming working. Add Now Playing UI.

--- app/src/main/java/insanityradio/insanityradio/FragmentNowPlaying.java
+++ app/src/main/java/insanityradio/insanityradio/FragmentNowPlaying.java
@@ -6,7 +6,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class FragmentNowPlaying extends Fragment {
+import co.mobiwise.library.RadioListener;
+import co.mobiwise.library.RadioManager;
+
+public class FragmentNowPlaying extends Fragment implements RadioListener {
+    private RadioManager radioManager;
+
     public static FragmentNowPlaying newInstance() {
         FragmentNowPlaying fragment = new FragmentNowPlaying();
         return fragment;
@@ -16,9 +21,31 @@ public class FragmentNowPlaying extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         setRetainInstance(true);
 
-        DataModel.updateData(getActivity());
+        radioManager = RadioManager.with(getActivity());
+        radioManager.connect();
+        radioManager.registerListener(this);
 
         View view = inflater.inflate(R.layout.fragment_nowplaying, container, false);
         return view;
+    }
+
+    @Override
+    public void onRadioConnected() {
+        radioManager.startRadio("http://stream.insanityradio.com:8000/insanity320.mp3");
+    }
+
+    @Override
+    public void onRadioStarted() {
+
+    }
+
+    @Override
+    public void onRadioStopped() {
+
+    }
+
+    @Override
+    public void onMetaDataReceived(String s, String s1) {
+        DataModel.updateData(getActivity());
     }
 }

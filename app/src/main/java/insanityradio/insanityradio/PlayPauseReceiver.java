a00844c2b200bdb52be59b7db41fcbf5ad7f1cc2
Dylan Maryk <dylan@dylanmaryk.com>
2015-09-07 19:17:47 +0100

    Restart main activity if necessary.

--- app/src/main/java/insanityradio/insanityradio/PlayPauseReceiver.java
+++ app/src/main/java/insanityradio/insanityradio/PlayPauseReceiver.java
@@ -7,6 +7,13 @@ import android.content.Intent;
 public class PlayPauseReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        FragmentNowPlaying.getInstance().playPauseButtonTapped(false);
+        try {
+            FragmentNowPlaying.getInstance().playPauseButtonTapped(false);
+        } catch (NullPointerException e) {
+            Intent startActivityIntent = new Intent(context.getApplicationContext(), MainActivity.class);
+            startActivityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            context.startActivity(startActivityIntent);
+        }
     }
 }
